3c7de3a73aaba60900f3f77dfcd056ab29a56ade
Tad Cordle <tad.cordle@gmail.com>
2020-03-15 12:22:13 -0400

    Update upload script

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
 
 REM Small script I use to upload bot to a raspberry pi
-scp -r bob.js elo_fix.js categories.js config.json emotes.json ubuntu@192.168.1.163:~/bingo-bot/
+scp -r bob.js fun.js elo_fix.js categories.js config.json emotes.json ubuntu@192.168.1.163:~/bingo-bot/
 ssh ubuntu@192.168.1.163
